0a15b00fe4f4e5de7f821a7fd9aff7ea326a08e0
ELAYOOBI <elayoubiabdelkarim@gmail.com>
2023-08-31 01:16:27 +0100

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string to print
+ */
+void _puts_recursion(char *str)
+{
+	if (*str == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*str);
+	_puts_recursion(str + 1);
+}
